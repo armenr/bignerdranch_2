b80d829de38a7a0566da30620142553194f9aab3
Armen Rostamian <arostamian@ulive.com>
2016-06-28 12:13:01 -0400

    truty response needed for roaster, UPCASE country input before validation

--- app/models/coffee_bean.rb
+++ app/models/coffee_bean.rb
@@ -3,5 +3,14 @@ class CoffeeBean < ActiveRecord::Base
   belongs_to :origin
   has_one :tasting_note
 
-validates :name, presence: true
+  validates :name, presence: true
+  validates :roasted_on, presence: true,
+            if: :roaster_present?
+
+  private
+
+  def roaster_present?
+    roaster.present?
+  end
+
 end
--- app/models/origin.rb
+++ app/models/origin.rb
@@ -2,8 +2,17 @@ class Origin < ActiveRecord::Base
   has_many :coffee_beans
   has_many :roasters, through: :coffee_beans
 
-validates :country, format: {
-    with: /\A[A-Z]{3}\z/,
-    message: 'must be an ISO-3166 3-character country code'
-}
+  validates :country, format: {
+      with: /\A[A-Z]{3}\z/,
+      message: 'must be an ISO-3166 3-character country code'
+  }
+
+  before_validation :upcase_country
+
+  private
+
+  def upcase_country
+    self.country = country.upcase
+  end
+
 end
