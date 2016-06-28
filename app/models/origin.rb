6a1288548159876213de03ae291289ded9e6e74b
Armen Rostamian <arostamian@ulive.com>
2016-06-28 12:10:13 -0400

    country code constraints

--- app/models/origin.rb
+++ app/models/origin.rb
@@ -1,4 +1,9 @@
 class Origin < ActiveRecord::Base
   has_many :coffee_beans
   has_many :roasters, through: :coffee_beans
+
+validates :country, format: {
+    with: /\A[A-Z]{3}\z/,
+    message: 'must be an ISO-3166 3-character country code'
+}
 end
